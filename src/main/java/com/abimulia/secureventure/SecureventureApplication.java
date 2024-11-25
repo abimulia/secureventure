bb3e749dbea970425d49c59f160f13c276509569
Abi Mulia <abimulia@outlook.com>
2024-11-25 11:18:08 +0700

    add initial project

---
+++ src/main/java/com/abimulia/secureventure/SecureventureApplication.java
@@ -0,0 +1,13 @@
+package com.abimulia.secureventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abimulia/secureventure/SecureventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abimulia.secureventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
